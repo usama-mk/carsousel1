a0c5ccf683aabba4d56f7328f6329fb74e78f52b
Usama Muhammad <usamamuhammad45678@gmail.com>
2020-10-07 22:28:43 +0500

    Updates

---
+++ precache-manifest.76071fa4b90e49412851718b48408cc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21720a79ef8ffa486b5836b12016b667",
+    "url": "/carsousel1/index.html"
+  },
+  {
+    "revision": "258c616ea82d46f66e7c",
+    "url": "/carsousel1/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "21d67d0c86319ec00b88",
+    "url": "/carsousel1/static/css/main.0e421966.chunk.css"
+  },
+  {
+    "revision": "258c616ea82d46f66e7c",
+    "url": "/carsousel1/static/js/2.6ac20fe3.chunk.js"
+  },
+  {
+    "revision": "999b2829de03f03f5ebf70720cbcd78e",
+    "url": "/carsousel1/static/js/2.6ac20fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21d67d0c86319ec00b88",
+    "url": "/carsousel1/static/js/main.7b7a57ed.chunk.js"
+  },
+  {
+    "revision": "809dacba20f2aa18dac3",
+    "url": "/carsousel1/static/js/runtime-main.e8a16129.js"
+  }
+]);
